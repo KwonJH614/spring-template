c4f807a08b6a90af806f1f9d4b6901d140c281c1
KwonJH614 <madogkwon@gmail.com>
2026-03-23 13:56:41 +0900

    refactor: UserDetailsService 인터페이스 및 미사용 코드 제거

--- src/main/java/com/gbsw/template/global/security/service/CustomUserDetailsService.java
+++ src/main/java/com/gbsw/template/global/security/service/CustomUserDetailsService.java
@@ -6,25 +6,15 @@ import com.gbsw.template.global.exception.CustomException;
 import com.gbsw.template.global.exception.ErrorCode;
 import com.gbsw.template.global.security.CustomUserPrincipal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService {
 
     private final UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-        return new CustomUserPrincipal(user.getId(), user.getUsername(), user.getPassword(), user.getRole());
-    }
-
-    public UserDetails loadUserByUserId(Long userId) {
+    public CustomUserPrincipal loadUserByUserId(Long userId) {
         UserEntity user = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
         return new CustomUserPrincipal(user.getId(), user.getUsername(), user.getPassword(), user.getRole());
